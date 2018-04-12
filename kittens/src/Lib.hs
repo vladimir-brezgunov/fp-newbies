adfb80d4c3985b33a49408e0e9a946a518906097
Yury Gorshkov <y.s.gorshkov@gmail.com>
2018-04-12 12:05:34 +0300

    12.04.18: Refactoring and a bit of development

--- kittens/src/Lib.hs
+++ kittens/src/Lib.hs
@@ -1,3 +1,5 @@
+module Lib where
+
 import System.Random
 
 data Card = Explosion | Defuse | Favor | Skip | Shuffle | Attack |
@@ -7,7 +9,13 @@ type Kind = String
 
 type Hand = [Card]
 
-data Table = Table { tableDeck :: [Card], tableDiscard :: [Card], tableHands :: [Hand] }
+type Deck = [Card]
+
+data Table = Table { tableDeck :: Deck,
+                     tableDiscard :: [Card],
+                     tableHands :: [Hand],
+                     cardsInTurn :: [Card],
+                     currentPlayer :: Int }
   deriving Show
 
 defaultDeck = unpack [(Explosion, 4), (Defuse, 6), (Favor, 4), (Skip, 4),
@@ -15,24 +23,31 @@ defaultDeck = unpack [(Explosion, 4), (Defuse, 6), (Favor, 4), (Skip, 4),
                (NoInstruction "1", 4), (NoInstruction "2", 4), (NoInstruction "3", 4),
                (NoInstruction "4", 4), (NoInstruction "5", 4)]
 
-unpack :: [(Card, Int)] -> [Card]
-unpack ((card, count):list) = replicate count card ++ unpack list
-unpack [] = []
-
-shuffle :: StdGen -> [Card] -> [Card]
+unpack :: [(Card, Int)] -> Deck
+--unpack = concatMap (\ (card, count) -> replicate count card)
+unpack = concatMap (uncurry $ flip replicate)
+  
+shuffle :: StdGen -> Deck -> Deck
 shuffle _ [] = []
 shuffle stdGen cards = randomCard:shuffle newGen rest where
   (left, randomCard:right) = splitAt index cards
   rest = left ++ right
   (index, newGen) = randomR (0, length cards - 1) stdGen
 
-distributeDeck :: Int -> [Card] -> Table
---distributeDeck _ [] = error "Where are my cards?"
---distributeDeck numPlayers _ | numPlayers < 2 = error "Get me players, please"
-distributeDeck 0 cards = Table cards [] []
-distributeDeck numPlayers cards = Table deck [] hands where
+distributeDeck :: Int -> [Card] -> (Deck, [Hand])
+distributeDeck 0 cards = (cards, [])
+distributeDeck numPlayers cards = (deck, firstHand:hands) where
   numCards = 5
   (firstHand, restCards) = splitAt numCards cards
-  restTable = distributeDeck (numPlayers - 1) restCards
-  deck = tableDeck restTable
-  hands = firstHand : tableHands restTable
+  (deck, hands) = distributeDeck (numPlayers - 1) restCards
+
+makeGame :: StdGen -> Int -> Table
+makeGame _ playersNum | playersNum < 2 = error "Get me players, please"
+                      | playersNum > 5 = error "Too many players!"
+makeGame gen playersNum = Table deck [] hands [] startPlayer where
+  (deck, hands) = distributeDeck playersNum $ shuffle gen defaultDeck
+  startPlayer = fst $ randomR (0, playersNum - 1) gen 
+
+resolveTurn :: Table -> Table
+resolveTurn (Table (topCard:deck) discard hands playedCards currentPlayer) =
+  Table deck (discard ++ playedCards) hands [] $ currentPlayer + 1
